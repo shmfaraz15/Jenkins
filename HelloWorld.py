e56f3f668c0c1a715a2eaa48d5c7c5b2ee646fa8
faraz <shm.faraz@15gmail.com>
2023-02-27 23:47:32 +0530

    Updated

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
-print("Hello World Demo...19 Jan 2023 \n")
-print("Hello World Demo...19 Jan 2023 \n")
+
 print("Hello World Demo...27 Feb 2023 \n")
 print("Hello World Demo...27 Feb 2023 \n")
